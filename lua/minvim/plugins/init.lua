0e883f1d45085f0ca4540e1de9dd06558d217c06
Ciprian Constantin <ciprianfc@gmail.com>
2024-04-20 17:16:13 +0300

    formatting ...

--- lua/minvim/plugins/init.lua
+++ lua/minvim/plugins/init.lua
@@ -1,7 +1,5 @@
 -- some ggeneric settigns
 return {
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-
-
+	-- "gc" to comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} },
 }
