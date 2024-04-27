e930a1a04cb03cff9f547646ba6d8c78f67e5af0
Ciprian Constantin <ciprianfc@gmail.com>
2024-04-27 07:41:51 +0300

    prettier lualine

--- lua/minvim/plugins/lualine.lua
+++ lua/minvim/plugins/lualine.lua
@@ -2,12 +2,32 @@ return {
 	-- Set lualine as statusline
 	"nvim-lualine/lualine.nvim",
 	-- See `:help lualine.txt`
-	opts = {
-		options = {
-			icons_enabled = false,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+
+	config = function()
+		local lualine = require("lualine")
+		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+
+		local options = {
+			icons_enabled = true,
 			theme = "onedark",
-			component_separators = "|",
-			section_separators = "",
-		},
-	},
+			-- component_separators = "|",
+			-- section_separators = "",
+		}
+		lualine.setup({
+			options = options,
+			sections = {
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+			},
+		})
+	end,
 }
