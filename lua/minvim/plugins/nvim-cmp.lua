e76594edaad5b9715e416b2b20c030cc49658cac
Ciprian Constantin <ciprianfc@gmail.com>
2024-04-27 09:39:34 +0300

    diagnostics keymaps, better lualine

--- lua/minvim/plugins/nvim-cmp.lua
+++ lua/minvim/plugins/nvim-cmp.lua
@@ -33,6 +33,7 @@ return {
 		--  into multiple repos for maintenance purposes.
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer", -- source for text in buffer
 		-- { 'roobert/tailwindcss-colorizer-cmp.nvim', config = true },
 	},
 	config = function()
