c8c162fa5bc7af181836b32ccbc62a54918d880e
Ciprian Constantin <ciprianfc@gmail.com>
2024-05-04 14:02:39 +0300

    working tailwind colors

--- lua/minvim/plugins/nvim-cmp.lua
+++ lua/minvim/plugins/nvim-cmp.lua
@@ -34,7 +34,7 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-buffer", -- source for text in buffer
-		-- { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+		{ "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
 		-- "luckasRanarison/tailwind-tools.nvim",
 		"onsails/lspkind.nvim", -- vs-code like pictograms
 	},
@@ -110,13 +110,16 @@ return {
 			-- configure lspkind for vs-code like pictograms in completion menu
 			formatting = {
 				format = lspkind.cmp_format({
-					maxwidth = 50,
-					ellipsis_char = "...",
-					-- before = require("tailwind-tools.cmp").lspkind_format,
-					-- before = function(entry, item)
-					-- 	-- format_kinds(entry, item) -- add icons
-					-- 	return require("tailwindcss-colorizer-cmp").formatter(entry, item)
-					-- end,
+					-- mode = "symbol", -- show only symbol annotations
+					maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+					-- can also be a function to dynamically calculate max width such as
+					-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+					show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+					before = function(entry, item)
+						-- format_kinds(entry, item) -- add icons
+						return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+					end,
 				}),
 			},
 		})
