bf7c2d5729e5150a8c6ad8ba159d1fba83af2d4f
Ciprian Constantin <ciprianfc@gmail.com>
2024-06-12 21:18:36 +0300

    added rust and package updates

--- lua/minvim/plugins/lspconfig.lua
+++ lua/minvim/plugins/lspconfig.lua
@@ -183,7 +183,7 @@ return {
 					},
 				},
 				-- pyright = {},
-				-- rust_analyzer = {},
+				rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				tsserver = {},
 				html = {},
